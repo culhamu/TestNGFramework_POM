4ce5478918ac382f8118400b351c1fca0f5df856
Mustafa Culha <merguven753@gmail.com>
2023-03-01 12:29:22 +0300

    11. Gun Sonu

---
+++ src/test/java/tests/day11_POM_assertions/C02_PositiveLoginTesti.java
@@ -0,0 +1,33 @@
+package tests.day11_POM_assertions;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QdPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C02_PositiveLoginTesti {
+
+    @Test
+    public void positiveLoginTesti(){
+        //qd anasayfaya gidin
+        Driver.getDriver().get(ConfigReader.getProperty("qualityUrl"));
+
+        //Login butonuna basın
+        QdPage qdPage=new QdPage();
+        qdPage.ilkLoginLinki.click();
+
+        //geçerli kullanıcı adı ve password ile giriş yapın
+        qdPage.emailKutusu.sendKeys(ConfigReader.getProperty("qdGecerliUsername"));
+        qdPage.passwordKutusu.sendKeys(ConfigReader.getProperty("qdGecerliPassword"));
+        qdPage.loginButonu.click();
+
+        //basarili bir şekilde giriş yapıldığonı test edin
+        Assert.assertTrue(qdPage.basariliGirisElementi.isDisplayed());
+
+
+        Driver.closeDriver();
+
+    }
+}
---
+++ src/test/java/tests/day11_POM_assertions/C03_NegatifLoginTesti.java
@@ -0,0 +1,12 @@
+package tests.day11_POM_assertions;
+
+public class C03_NegatifLoginTesti {
+    //1- https://www.qualitydemy.com/ anasayfasina gidin
+    //2- login linkine basin
+    //3- Kullanvalid 3 farkli test method’u olusturun.
+    //- gecerli username, gecersiz password
+    //- gecersiz username, gecerli password
+    //- gecersiz username, gecersiz password.
+    //4- Login butonuna basarak login olun
+    //5- Basarili olarak giris yapilamadigini test edin
+}
